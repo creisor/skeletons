df1fd12f65b67928182f95c56e25fca47f821fad
Chris Reisor <creisor@gmail.com>
2017-01-05 10:31:28 -0600

    Update skel.rb
    Make an app class

--- skel.rb
+++ skel.rb
@@ -21,33 +21,28 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
 require 'logger'
 require 'optparse'
 
-ME = File.basename $0
+class App
+  def initialize
+    @log = Logger.new(STDERR)
+    @log.level = Logger::WARN
+    @options = {
+      :verbose => false,
+    }
+  end
 
-$Log = Logger.new(STDERR)
-$Log.level = Logger::WARN
-
-options = {
-  :verbose => false,
-  :something => 'default',
-}
-
-
-def main
-  opts = OptionParser.new do |opts|
-    opts.on('-h', '--help', 'display this help text and exit') do
+  def main
+    opts = OptionParser.new do |opts|
+      opts.on('-v', '--verbose', 'write debug output to stderr') do
+        @options[:verbose] = true
+        @log.level = Logger::DEBUG
+        @log.debug "VERBOSE enabled"
+      end
+      opts.parse!(ARGV)
     end
-    opts.on('-f', '--somearg SOMETHING', 'option that takes an argument') do |something|
-      options[:something] = something
-    end
-    opts.on('-v', '--verbose', 'write debug output to stderr') do
-      options[:verbose] = true
-      $Log.level = Logger::DEBUG
-      $Log.debug "VERBOSE enabled"
-    end
-    opts.parse!(ARGV)
   end
 end
 
 if $0 == __FILE__
-  main
+  app = App.new
+  app.main
 end
